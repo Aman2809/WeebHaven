2e4978dbf15b5c074468a9a392cb813482758dab
subham008 <shaw.subham14.05.2002@gmail.com>
2024-02-08 15:02:56 +0530

    spring project created with maven , war  , jdk 17 and only spring web dependency

---
+++ weebhaven/src/main/java/com/anime/weebhaven/weebhaven/WeebhavenApplication.java
@@ -0,0 +1,13 @@
+package com.anime.weebhaven.weebhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeebhavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeebhavenApplication.class, args);
+	}
+
+}
---
+++ weebhaven/src/test/java/com/anime/weebhaven/weebhaven/WeebhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.weebhaven.weebhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeebhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
